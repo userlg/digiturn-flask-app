829ac4a025b59d1eb64d02b8f695e1ecad9c2333
userlg <guipeluis90@gmail.com>
2021-07-25 17:38:07 -0400

    New style and logo

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.env = 'development'
 app.debug = True
+app.secret_key='Greenkey'
 port = 7000
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/task.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 from app import views
 
