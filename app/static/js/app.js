cd94550aca921d8808fd8d853879783f1784c789
userlg <guipeluis90@gmail.com>
2021-07-27 09:47:03 -0400

    New js file with modal managment

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,7 +1,25 @@
 
 const btnNormal = document.getElementById('normal')
 
-btnNormal.addEventListener('click', () => {
-  console.log('Boton presionado')
-  window.open('/', 'MyWindow', 'width=500,height=500,scrollbars=no, margin=auto')
+const btnTicketModal = document.getElementById('btnTicket')
+
+btnNormal.addEventListener('click', (e) => {
+const modal = document.getElementById('container_modal');
+console.log('Abriendo Modal : Cliente Normal');
+modal.style.display='flex';
+
+e.preventDefault();
+});
+
+
+btnTicketModal.addEventListener('click', (e) => {
+  const modal = document.getElementById('container_modal');
+  console.log('Espero 4 segundos')
+  setTimeout(() => {
+    
+    modal.style.display='none';},4000)
+
+
+e.preventDefault();
+
 })
