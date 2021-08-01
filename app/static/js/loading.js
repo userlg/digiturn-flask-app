56aafb842ffbb27c69a7fecad52046ee1d2413fd
userlg <guipeluis90@gmail.com>
2021-08-01 19:44:03 -0400

    Added loading.js file

---
+++ app/static/js/loading.js
@@ -0,0 +1,6 @@
+
+//Carga de Pagina protector de pantalla Loading 
+window.onload = function() {
+  let container = document.getElementById('loading_container');
+  container.style.display = 'none';
+}
